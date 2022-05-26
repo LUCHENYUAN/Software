1cfde25bd7471806e9ac9b438adc379f1f99c6b4
LUCHENYUAN <827887998@qq.com>
2022-05-26 23:31:11 +0800

    再次修改数据库

--- table_test.sql
+++ table_test.sql
@@ -10,94 +10,99 @@ create table User
    avatar varchar(20), #用户头像的地址
    password varchar(80) not null,
    mail varchar(30) not null,
-   preferrence varchar(50) not null,
-   gender varchar(6) not null check(gender in ('male','female')),
-   phone varchar(11) not null 
+   preferrence varchar(50) not null defaule '无',
+   gender varchar(6) not null check(gender in ('male','female')) default 'male',
+   phone varchar(11) not null,
+   black tinyint not null default 0,
+   send_mail tinyint default 0,
+   send_way varchar(8) default 'mail'
 );
 
 
 drop table if exists Game;
-create table Game
+create table letscode.game
 (
-   game_id int(11) primary key not null auto_increment,
-   game_name varchar(50) not null,
-   game_start_time datetime not null,
-   game_end_time datetime,
-   duration varchar(11),
-   checked tinyint default 0,
-   website varchar(100) not null,
-   game_type varchar(20) not null, #周赛/月赛
-   level_  tinyint not null,#难易度 level是mysql保留字所以加下划线 
-   platform varchar(20) ,#平台，可空
-
-   check ((game_end_time is not null) or (duration is not null))
+    game_id         int auto_increment
+        primary key,
+    game_name       varchar(50)       not null,
+    game_start_time datetime          not null,
+    game_end_time   datetime          null,
+    duration        varchar(11)       null,
+    checked         tinyint           null,
+    website         varchar(100)      not null,
+    game_type       varchar(20)       null,
+    level_          tinyint           null,
+    platform        varchar(20)       null,
+    cust_reason     varchar(20)       null,
+    cust_user_id    int     default 0 null,
+    cust_public     tinyint default 0 null,
+    check ((`game_end_time` is not null) or (`duration` is not null))
 );
-alter table Game  modify COLUMN checked tinyint;
-alter table Game  modify COLUMN game_type varchar(20);
-alter table Game  modify COLUMN level_ tinyint;
 
 #帖子表
 drop table if exists Post;
-create table Post
+create table letscode.post
 (
-post_id int(11)  primary key not null auto_increment,
-user_id int(11) not null, 
-headline varchar(50) not null,
-create_time datetime default now(),
-update_time datetime default now(),
-content mediumtext ,#varchar最多256字符，存不下
-drafted tinyint default 0,#是否草稿check
-checked tinyint default 0,#是否已审核check
-block_type tinyint not null,
-
-foreign key(user_id) references User(user_id)
+    post_id     int auto_increment
+        primary key,
+    user_id     int                                not null,
+    headline    varchar(50)                        not null,
+    create_time datetime default CURRENT_TIMESTAMP null,
+    update_time datetime default CURRENT_TIMESTAMP null,
+    content     mediumtext                         null,
+    drafted     tinyint  default 0                 null,
+    checked     tinyint  default 0                 null,
+    block_type  tinyint                            not null,
+    topped      tinyint  default 0                 null,
+    com_count   int      default 0                 null,
+    constraint post_ibfk_1
+        foreign key (user_id) references letscode.user (user_id)
 );
 
 
 #评论表
-drop table if exists Comment;
-create table Comment
+create table letscode.comment
 (
-comment_id int(11) primary key not null auto_increment,
-user_id int(11) not null,
-post_id int(11) not null,
-content text not null,
-reply_id int(11) default 0,#0则为原始评论，如果评论被回复，则保存评论的id
-agree_count int(11) default 0,
-oppo_count int(11) default 0,
-hidden tinyint default 0,
-create_time datetime default now(),
-
-foreign key(user_id) references User(user_id),
-foreign key(post_id) references Post(post_id)
+    comment_id  int auto_increment
+        primary key,
+    user_id     int                                not null,
+    post_id     int                                not null,
+    content     text                               not null,
+    reply_id    int                                null,
+    agree_count int      default 0                 null,
+    oppo_count  int      default 0                 null,
+    hidden      tinyint  default 0                 null,
+    create_time datetime default CURRENT_TIMESTAMP null,
+    constraint comment_ibfk_1
+        foreign key (user_id) references letscode.user (user_id),
+    constraint comment_ibfk_2
+        foreign key (post_id) references letscode.post (post_id)
 );
 
 #订阅表
 drop table if exists Subscribe;
-create table Subscribe
+create table letscode.subscribe
 (
-subscribe_id int(11) primary key not null auto_increment,
-user_id int(11) not null,
-game_type varchar(20) ,
-level_ tinyint ,
-platform varchar(20),
-
-foreign key(user_id) references User(user_id),
-
-#三者至少一个非空
-check ((game_type is not null) or (level_ is not null) or (platform is not null))
+    subscribe_id int auto_increment
+        primary key,
+    user_id      int         not null,
+    platform     varchar(40) not null,
+    constraint subscribe_ibfk_1
+        foreign key (user_id) references letscode.user (user_id)
 );
 
 #预约表
 drop table if exists Reserve;
-create table Reserve
+create table letscode.reserve
 (
-subscribe_id int(11) primary key not null auto_increment,
-user_id int(11) not null,
-game_id int(11) not null,
-
-foreign key(user_id) references User(user_id),
-foreign key(game_id) references Game(game_id)
+    reserve_id int auto_increment
+        primary key,
+    user_id    int not null,
+    game_id    int not null,
+    constraint reserve_ibfk_1
+        foreign key (user_id) references letscode.user (user_id),
+    constraint reserve_ibfk_2
+        foreign key (game_id) references letscode.game (game_id)
 );
 
 #info表
