486aecfcbd3b2261b5e20441291440ea46ddc596
LUCHENYUAN <827887998@qq.com>
2022-05-27 15:26:27 +0800

    tablesql

--- table_test.sql
+++ table_test.sql
@@ -20,99 +20,99 @@ create table User
 
 
 drop table if exists Game;
-create table letscode.game
+create table Game
 (
-    game_id         int auto_increment
-        primary key,
-    game_name       varchar(50)       not null,
-    game_start_time datetime          not null,
-    game_end_time   datetime          null,
-    duration        varchar(11)       null,
-    checked         tinyint           null,
-    website         varchar(100)      not null,
-    game_type       varchar(20)       null,
-    level_          tinyint           null,
-    platform        varchar(20)       null,
-    cust_reason     varchar(20)       null,
-    cust_user_id    int     default 0 null,
+   game_id int(11) primary key not null auto_increment,
+   game_name varchar(50) not null,
+   game_start_time datetime not null,
+   game_end_time datetime,
+   duration varchar(11),
+   checked tinyint default 0,
+   website varchar(100) not null,
+   game_type varchar(20) not null, #周赛/月赛
+   level_  tinyint not null,#难易度 level是mysql保留字所以加下划线 
+   platform varchar(20) ,#平台，可空
+   cust_reason     varchar(20)       null,
+    cust_user_id    int(11)     default 0 null,
     cust_public     tinyint default 0 null,
-    check ((`game_end_time` is not null) or (`duration` is not null))
+   
+   
+   check ((game_end_time is not null) or (duration is not null))
 );
+alter table Game  modify COLUMN checked tinyint;
+alter table Game  modify COLUMN game_type varchar(20);
+alter table Game  modify COLUMN level_ tinyint;
 
 #帖子表
 drop table if exists Post;
-create table letscode.post
+create table Post
 (
-    post_id     int auto_increment
-        primary key,
-    user_id     int                                not null,
-    headline    varchar(50)                        not null,
-    create_time datetime default CURRENT_TIMESTAMP null,
-    update_time datetime default CURRENT_TIMESTAMP null,
-    content     mediumtext                         null,
-    drafted     tinyint  default 0                 null,
-    checked     tinyint  default 0                 null,
-    block_type  tinyint                            not null,
-    topped      tinyint  default 0                 null,
-    com_count   int      default 0                 null,
-    constraint post_ibfk_1
-        foreign key (user_id) references letscode.user (user_id)
+post_id int(11)  primary key not null auto_increment,
+user_id int(11) not null, 
+headline varchar(50) not null,
+create_time datetime default now(),
+update_time datetime default now(),
+content mediumtext ,#varchar最多256字符，存不下
+drafted tinyint default 0,#是否草稿check
+checked tinyint default 0,#是否已审核check
+block_type tinyint not null,
+topped      tinyint  default 0                 null,
+com_count   int(11)      default 0                 null,
+
+foreign key(user_id) references User(user_id)
 );
 
 
 #评论表
-create table letscode.comment
+drop table if exists Comment;
+create table Comment
 (
-    comment_id  int auto_increment
-        primary key,
-    user_id     int                                not null,
-    post_id     int                                not null,
-    content     text                               not null,
-    reply_id    int                                null,
-    agree_count int      default 0                 null,
-    oppo_count  int      default 0                 null,
-    hidden      tinyint  default 0                 null,
-    create_time datetime default CURRENT_TIMESTAMP null,
-    constraint comment_ibfk_1
-        foreign key (user_id) references letscode.user (user_id),
-    constraint comment_ibfk_2
-        foreign key (post_id) references letscode.post (post_id)
+comment_id int(11) primary key not null auto_increment,
+user_id int(11) not null,
+post_id int(11) not null,
+content text not null,
+reply_id int(11) default 0,#0则为原始评论，如果评论被回复，则保存评论的id
+agree_count int(11) default 0,
+oppo_count int(11) default 0,
+hidden tinyint default 0,
+create_time datetime default now(),
+
+foreign key(user_id) references User(user_id),
+foreign key(post_id) references Post(post_id)
 );
 
 #订阅表
 drop table if exists Subscribe;
-create table letscode.subscribe
+create table Subscribe
 (
-    subscribe_id int auto_increment
-        primary key,
-    user_id      int         not null,
-    platform     varchar(40) not null,
-    constraint subscribe_ibfk_1
-        foreign key (user_id) references letscode.user (user_id)
+subscribe_id int(11) primary key not null auto_increment,
+user_id int(11) not null,
+
+platform varchar(40) not null,
+
+foreign key(user_id) references User(user_id)
+
+
 );
 
 #预约表
 drop table if exists Reserve;
-create table letscode.reserve
+create table Reserve
 (
-    reserve_id int auto_increment
-        primary key,
-    user_id    int not null,
-    game_id    int not null,
-    constraint reserve_ibfk_1
-        foreign key (user_id) references letscode.user (user_id),
-    constraint reserve_ibfk_2
-        foreign key (game_id) references letscode.game (game_id)
+subscribe_id int(11) primary key not null auto_increment,
+user_id int(11) not null,
+game_id int(11) not null,
+
+foreign key(user_id) references User(user_id),
+foreign key(game_id) references Game(game_id)
 );
 
-#info表
-create table letscode.info
+create table Info
 (
-    info_id     int(11) auto_increment
-        primary key,
-    user_id     int(11)     default 0                 null,
-    content     varchar(100)                       null,
-    headline    varchar(20)                        not null,
-    create_time datetime default CURRENT_TIMESTAMP null
-)
+    info_id     int(11) auto_increment   primary key,
+    user_id     int(11)      default 0,
+    content     varchar(100)  ,
+    headline    varchar(20)   not null,
+    create_time datetime default now()
+);
 
